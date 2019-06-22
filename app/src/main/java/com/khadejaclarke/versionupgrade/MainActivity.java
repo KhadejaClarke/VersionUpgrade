e639dde7f3aa53e39b992a36b61a26ad46107ede
KhadejaClarke <clarke.khadeja@gmail.com>
2019-06-23 05:55:46 +0800

    Initial commit

---
+++ app/src/main/java/com/khadejaclarke/versionupgrade/MainActivity.java
@@ -0,0 +1,32 @@
+package com.khadejaclarke.versionupgrade;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        progressBar = findViewById(R.id.progressBar);
+        showUpdateDialog();
+    }
+
+    private void showUpdateDialog(){
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("App version upgrade")
+                .setMessage("Would you like to upgrade now or later?")
+                .setPositiveButton("Now", (dialog, which) -> progressBar.setVisibility(View.VISIBLE))
+                .setNegativeButton("Later", (dialog, which) -> textView.setVisibility(View.VISIBLE))
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
